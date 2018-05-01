0edb6ba96d5b485231748381c717db5f86cfd947
lecheton <38864755+lecheton@users.noreply.github.com>
2018-04-30 20:17:39 -0400

    Add files via upload

---
+++ Heartbeat/Heartbeat.m
@@ -0,0 +1,17 @@
+function [Heartbeat,t] = Heartbeat(HR,Freq,pulse_width,Amp,duration)
+%UNTITLED3 Summary of this function goes here
+%   Heartbeat is measured in beats per minute
+%   Sample frequency (fs) sets the sample frequency in Hertz. (Generally
+%    100-500 Hz
+%   Pulse width is the time for the QRS wave
+%   Amp in in microVolts 
+%   Duration is the intended time of the program
+Fs = Freq;
+
+delay = (60/HR);
+t = 0 : 1/Fs : duration; 
+d = 0 : delay : duration; 
+Heartbeat = Amp*pulstran(t,d,'tripuls',pulse_width);
+plot(t,Heartbeat);
+end
+
